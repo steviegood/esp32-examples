8cf95fcb2c71bc30ac4daabfd3db080bdb18d7d1
Steven Good <stevieg.bitbucket@gmail.com>
2025-12-23 23:23:04 +0000

    Added deepsleep and wake-up based on activating pin 4 using a button

--- micropython/examples/wifi_with_mqtt.py
+++ micropython/examples/wifi_with_mqtt.py
@@ -1,9 +1,10 @@
+import esp32
 import machine
 import network
+import ubinascii
 import uos
 import umqtt.simple
 import time
-
 from machine import Pin
 from utime import sleep
 
@@ -39,8 +40,6 @@ def host_info():
     result['unique_id'] = machine.unique_id().hex()
     return result
     
-    
-
 
 # See https://docs.micropython.org/en/latest/esp32/quickref.html#general-board-control
 def connect_wifi(credentials):
@@ -51,8 +50,10 @@ def connect_wifi(credentials):
         wlan.connect(credentials.get("WIFI_SSID"), credentials.get("WIFI_KEY"))
         while not wlan.isconnected():
             machine.idle()
-    print('WIFI IPv4 address: ', wlan.ipconfig('addr4'))
-    print('WIFI MAC address: ', wlan.config('mac'))
+    print('WIFI connectivity:')
+    print(f'\tWIFI IPv4 address(es): {", ".join(wlan.ipconfig("addr4"))}')
+    print(f'\tWIFI MAC address: ' + ubinascii.hexlify(wlan.config('mac'),':').decode().upper())
+    return wlan
 
 
 # See https://mpython.readthedocs.io/en/v2.2.1/library/mPython/umqtt.simple.html
@@ -71,34 +72,55 @@ def connect_mqtt(credentials):
 
 
 # Main program
+reason = machine.wake_reason()
+print(f"Wake reason: {reason}")
+
 hostinfo = host_info()
+print('Host information:')
 for k in hostinfo:
-    print(k+': '+hostinfo.get(k))
+    print(f'\t{k}: {hostinfo.get(k)}')
     
 secrets_filename='secrets.txt'
-credentials = read_secrets(secrets_filename)
-for k in credentials:
-    print('Found credential: '+k)
+secrets = read_secrets(secrets_filename)
+print('Secrets:')
+for k in secrets:
+    print(f'\t{k}: {secrets.get(k)}')
     
-connect_wifi(credentials)
-mqtt = connect_mqtt(credentials)
+wifi = connect_wifi(secrets)
+mqtt = connect_mqtt(secrets)
 
-topic = credentials.get('MQTT_PUBLISH_TOPIC')
-print('Using publish topic '+topic)
+topic = secrets.get('MQTT_PUBLISH_TOPIC')
+print(f'Using publish topic: {topic}')
 
 mqtt.publish(topic,'Initiated connection from esp32',0)
 
-delay = 1         # seconds
-pin_number = 5    # pin for LED
+light_on_time = 1         # seconds
+light_off_time = 2        # seconds
+deep_sleep_time = 60      # seconds
+light_iterations = 5      # cycle count
+led_pin_number = 5               # pin for LED
+button_pin_number = 4            # pin for button
 
-led = Pin(pin_number, Pin.OUT)
 
-while True:
+led = Pin(led_pin_number, Pin.OUT)
+button = Pin(button_pin_number, Pin.IN)
+
+esp32.wake_on_ext0(pin = button, level = esp32.WAKEUP_ANY_HIGH)
+
+i = 0
+while i < light_iterations:
     led.on()
     mqtt.publish(topic, 'Light ON',0)
-    sleep(delay)
+    sleep(light_on_time)
     led.off()
     mqtt.publish(topic, 'Light OFF',0)
-    sleep(delay*2)
+    sleep(light_off_time)
+    i = i+1
+
+print('Preparing for deepsleep...')
+mqtt.publish(topic, 'Entering deepsleep...',0)
+sleep(5)   # sleeping so MQTT message can be published to broker
+print('Entering deepsleep')
+machine.deepsleep(deep_sleep_time*1000)
 
 
